299149a7eb2665e788995dd74131f222d0050b32
virghiu <virghiu@gmail.com>
2023-10-09 16:12:57 -0400

    feat: SingleColor Component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import ColorList from './components/ColorList'
 import Form from './components/Form'
+import Values from 'values.js'
 
 const App = () => {
+  const [colors, setColors] = useState(new Values('#fa7264').all(10))
+
   return (
     <main>
       <Form />
-      <ColorList />
+      <ColorList colors={colors} />
     </main>
   )
 }
--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,5 +1,15 @@
-const SingleColor = () => {
-  return <div>SingleColor</div>
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color
+
+  return (
+    <article
+      className={index > 10 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='percent-value'>#{hex}</p>
+    </article>
+  )
 }
 
 export default SingleColor
